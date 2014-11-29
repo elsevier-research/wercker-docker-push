57be2eab2ea75e7e0b5caa4ba7ac7f1e78dd9088
Nicolas Huray <nicolas.huray@1science.org>
2014-11-28 19:17:57 -0500

    Docker push step

---
+++ README.md
@@ -0,0 +1,39 @@
+# wercker-docker-build
+
+This wercker step just push your package to a [Docker](https://docs.docker.com/reference/commandline/cli/#push) registry.
+
+This step must be used with a wercker box build with [Docker Support](http://devcenter.wercker.com/articles/docker)
+
+
+## Versions
+
+| Release date | Step version | Docker version |
+| -------------| -------------| ---------------|
+| 2014-11-28   | 1.0.0        | 1.3.1          |
+
+
+## Options
+
+* `image` (required) Image to push to the registry
+* `registry` (optional) Docker registry server, if no server is specified "https://index.docker.io/v1/" is the default.
+* `username` (required) Username needed to login to the Docker registry
+* `password` (required) Password needed to login to the Docker registry
+* `email` (required) Email needed to login to the Docker registry
+
+## Example
+
+
+The following example push a [previously built](https://github.com/nhuray/wercker-docker-build) docker image to a
+private [tutum.co](https://www.tutum.co/) registry :
+
+```
+build:
+  steps:
+    ...
+    - nhuray/docker-push:
+        image: tutum.co/nhuray/myimage/${WERCKER_GIT_COMMIT:0:7}
+        registry: tutum.co
+        email: nicolas.huray@gmail.com
+        password: *****
+        username: nhuray
+```
---
+++ run.sh
@@ -0,0 +1,93 @@
+#!/bin/sh
+set -e
+
+if [ -z "$WERCKER_DOCKER_PUSH_IMAGE" ]; then
+  fail 'A Docker image is required.'
+fi
+
+if [ -n "$WERCKER_DOCKER_PUSH_REGISTRY" ]; then
+  REGISTRY="$WERCKER_DOCKER_PUSH_REGISTRY"
+fi
+
+if [ -z "$WERCKER_DOCKER_PUSH_USERNAME" ]; then
+  fail 'A username is required to login to the registry'
+fi
+
+if [ -z "$WERCKER_DOCKER_PUSH_PASSWORD" ]; then
+  fail 'A password is required to login to the registry'
+fi
+
+if [ -z "$WERCKER_DOCKER_PUSH_EMAIL" ]; then
+  fail 'An email is required to login to the registry'
+fi
+
+
+type_exists() {
+  if [ $(type -P $1) ]; then
+    return 0
+  fi
+  return 1
+}
+
+# Check for Docker
+if ! type_exists 'docker'; then
+  fail 'Docker is not installed on this box.'
+  info 'Please use a box with docker installed : http://devcenter.wercker.com/articles/docker'
+  exit 1
+fi
+
+# Check a Dockerfile is present
+if [ ! -f 'Dockerfile' ]; then
+  fail 'A Dockerfile is required.'
+  info 'Please create a Dockerfile : https://docs.docker.com/reference/builder/'
+  exit 1
+fi
+
+
+set +e
+
+USERNAME="--username $WERCKER_DOCKER_PUSH_USERNAME"
+PASSWORD="--password $WERCKER_DOCKER_PUSH_PASSWORD"
+EMAIL="--email $WERCKER_DOCKER_PUSH_EMAIL"
+
+# Login to the registry
+info 'login to the docker registry'
+DOCKER_LOGIN="docker login $USERNAME $PASSWORD $EMAIL $REGISTRY"
+debug "$DOCKER_LOGIN"
+DOCKER_LOGIN_OUTPUT=$($DOCKER_LOGIN)
+
+if [[ $? -ne 0 ]];then
+  warn $DOCKER_LOGIN_OUTPUT
+  fail 'docker login failed';
+else
+  success 'docker login succeed';
+fi
+
+# Push the docker image
+info 'pushing docker image'
+
+DOCKER_PUSH="docker push $WERCKER_DOCKER_PUSH_IMAGE"
+debug "$DOCKER_PUSH"
+DOCKER_PUSH_OUTPUT=$($DOCKER_PUSH)
+
+if [[ $? -ne 0 ]];then
+  warn $DOCKER_PUSH_OUTPUT
+  fail 'docker push failed';
+else
+  success 'docker push succeed';
+fi
+
+# Logout from the registry
+info 'logout to the docker registry'
+DOCKER_LOGOUT="docker logout $REGISTRY"
+debug "$DOCKER_LOGOUT"
+DOCKER_LOGOUT_OUTPUT=$($DOCKER_LOGOUT)
+
+if [[ $? -ne 0 ]];then
+  warn $DOCKER_LOGOUT_OUTPUT
+  fail 'docker logout failed';
+else
+  success 'docker logout succeed';
+fi
+
+set -e
